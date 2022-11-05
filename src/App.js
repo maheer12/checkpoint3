23bb87710136f1fb911086bb90389904f2f0ca2c
maheer12 <maher.cherbib@gmail.com>
2022-11-05 14:45:30 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import react from 'react' ; 
-import {BrowserRouter as Router , Route }from ' react-router-dom'
-import home from './home'
-import about from './about'
-import front from'./layout/front'
-import back from'./layout/back'
-
-const AppRoute = ({component : Component,layout:layout,...rest})=> (
-  <Route{...rest}render = {props =>(
-  <layout> <component {...props}></component> </layout>)}></Route>
-
 function App() {
-  return (
-
-    <Router>
-      <AppRoute path ="/" exact layout={front} Component={home} />
-      <AppRoute path ="/about" layout={back} Component={about} />
-      </Router>
-    
-  );
+  return <div><form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input
+      type="email"
+      class="form-control"
+      id="exampleInputEmail1"
+      aria-describedby="emailHelp"
+      placeholder="Enter email"
+    ></input>
+    <small id="emailHelp" class="form-text text-muted">
+      We'll never share your email with anyone else.
+    </small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input
+      type="password"
+      class="form-control"
+      id="exampleInputPassword1"
+      placeholder="Password"
+    ></input>
+  </div>
+  <div class="form-check">
+    <input
+      type="checkbox"
+      class="form-check-input"
+      id="exampleCheck1"
+    ></input>
+    <label class="form-check-label" for="exampleCheck1">
+      Check me out
+    </label>
+  </div>
+  <button type="submit" class="btn btn-primary">
+    Submit
+  </button>
+</form></div>;
 }
 
 export default App;
